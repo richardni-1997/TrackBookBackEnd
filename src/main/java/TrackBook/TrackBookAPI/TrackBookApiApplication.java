079467ccf94ac33bfcecf512f1c2642340487e37
rni <rni@xpanxion.com>
2021-05-13 10:28:40 -0400

    created Project

---
+++ src/main/java/TrackBook/TrackBookAPI/TrackBookApiApplication.java
@@ -0,0 +1,13 @@
+package TrackBook.TrackBookAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackBookApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackBookApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/TrackBook/TrackBookAPI/TrackBookApiApplicationTests.java
@@ -0,0 +1,13 @@
+package TrackBook.TrackBookAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackBookApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
